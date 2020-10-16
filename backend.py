0a1f5aa614626b8b33d0277efc4552bc6f2fa61b
marvin <marvin23m@gmail.com>
2020-10-16 13:31:27 +0100

    Add search functionality

--- backend.py
+++ backend.py
@@ -17,4 +17,24 @@ def insert(title, author, year, isbn):
     conn.close()
 
 
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def search(title='', author='', year='', isbn=''):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
 connect()
+insert("Moby Dick", "Marvin", 1953, 82323923)
+print(view())
